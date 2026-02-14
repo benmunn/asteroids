0edac4bb460d4fcfb52c6b7e58d50d19e0eee48f
benmunn <benmunn123@gmail.com>
2026-02-15 01:31:54 +0900

    asteroids shootable

--- asteroid.py
+++ asteroid.py
@@ -1,11 +1,20 @@
 from circleshape import CircleShape
 import pygame
 from constants import *
+from logger import log_event
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
         super().__init__(x, y, radius)
+    
     def draw(self, screen):
         pygame.draw.circle(screen, "white", self.position, self.radius, LINE_WIDTH)
+    
     def update(self, dt):
         self.position += dt * self.velocity
+
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        ##log_event("asteroid_split")
--- main.py
+++ main.py
@@ -43,6 +43,11 @@ def main():
                 log_event("player_hit")
                 print("Game over!")
                 sys.exit()
+            for shot in shots:
+                if ast.collides_with(shot):
+                    log_event("asteroid_shot")
+                    ast.split()
+                    shot.kill()
         pygame.display.flip()
 
 
